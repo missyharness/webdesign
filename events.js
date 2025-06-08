02df7d552de565ef48481f07f4f7856a56f695cb
missyharness <mh3854@drexel.edu>
2025-06-07 23:07:20 -0400

    Update events.js

--- events.js
+++ events.js
@@ -66,8 +66,7 @@ function showDayEvents(dateStr, cell) {
   events.forEach(ev => {
     const div = document.createElement('div');
     div.className = 'event-listing';
-    div.textContent = ev.title + ' (' + ev.start + ' - ' + ev.end + ')';
-    div.onclick = () => showEventModal(ev);
+    div.textContent = ev.title + ' (' + to12Hour(ev.start) + ' - ' + to12Hour(ev.end) + ')';    div.onclick = () => showEventModal(ev);
     dayEventsDiv.appendChild(div);
   });
   dayEventsDiv.style.display = 'block';
